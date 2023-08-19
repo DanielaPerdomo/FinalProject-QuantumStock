a9a6b27f0564d347c7f8b8eb3e1bec7cf97f79a3
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-08-19 03:15:43 +0000

    Diseno de Navbar, modificaciones en el componente y en page/home

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,15 +3,31 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+		<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  			<div class="container-fluid">
+			  <Link to="/">
+					<span className="navbar-brand mb-0 h1">
+						<i class="fa-solid fa-boxes-packing"></i><br/>
+						<strong> QuantumStorage </strong>
+					</span>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+    			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      				<span class="navbar-toggler-icon"></span>
+    			</button>
+    			<div class="collapse navbar-collapse position-relative" id="navbarSupportedContent">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+						<Link to="/aboutUs">
+							<button class="btn btn-outline-secondary border border-0 text-primary-emphasis m-2 position-absolute top-0 start-50 translate-middle-x" type="submit" href="#">Nosotros</button>
+						</Link>
+						</li>
+					</ul>
+						<Link to="/">
+							<button class="btn btn-outline-primary text-primary-emphasis m-2" type="submit">Registrarse</button>
+						</Link>
+						<Link to="/demo">
+							<button class="btn btn-outline-primary text-primary-emphasis m-2" type="submit">Iniciar Sesión</button>
+						</Link>
 				</div>
 			</div>
 		</nav>

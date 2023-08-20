a3d6ffc4ac6c02a0650a839e07d108469fc15967
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-08-20 15:24:49 +0000

    Se modifico el modelo User agregando nuevos campos requeridos

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,12 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    company_name = db.Column(db.String(250), unique=False, nullable=False)
+    rol_company = db.Column(db.String(250), unique=False, nullable=False)
+    password_hash = db.Column(db.String(800), unique=False, nullable=False)
+    salt = db.Column(db.String(800), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +17,9 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
             "email": self.email,
+            "company_name": self.company_name,
+            "rol_company": self.rol_company
             # do not serialize the password, its a security breach
         }

0e02d1c4c4aed170fdac928033a0b94c92f7bfbc
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-08-20 20:13:48 +0000

    Se elimino y se creo la base de datos 'exemple' con su nuevo modelo

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,34 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def handle_singup():
+    body = request.json
+    name = body.get("name")
+    email = body.get("email")
+    company_name = body.get("company_name")
+    rol_company = body.get("rol_company")
+    password_hash = body.get("password_hash")
+    salt = body.get("salt")
+
+    if name is None or email is None or company_name is None or rol_company is None or password_hash is None or salt is None:
+        return jsonify({
+            "message": "Name, email, company_name, rol_company, password_hash and salt are required"
+        }), 400
+    
+    new_user = User(name, email, company_name, rol_company, password_hash, salt)
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "an internal error occurred",
+            "error": error.args
+        }), 500
+    return jsonify({}), 201
+
+    

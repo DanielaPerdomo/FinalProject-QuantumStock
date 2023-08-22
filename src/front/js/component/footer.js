acfd88a85a496cdd7f13924064f9ec5f1ed74fbb
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-08-22 00:55:42 +0000

    arreglo del flux

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 export const Footer = () => (
 	<footer className="footer mt-auto text-center">
 		<div>
-			<span><i class="instagram fa-brands fa-instagram"></i></span>
-			<span><i class="xtwitter fa-brands fa-x-twitter"></i></span>
-			<span><i class="twitter fa-brands fa-twitter"></i></span>
-			<span><i class="facebook fa-brands fa-facebook"></i></span>
-			<span><i class="linkedin fa-brands fa-linkedin-in"></i></span>
+			<span><i className="instagram fa-brands fa-instagram"></i></span>
+			<span><i className="xtwitter fa-brands fa-x-twitter"></i></span>
+			<span><i className="twitter fa-brands fa-twitter"></i></span>
+			<span><i className="facebook fa-brands fa-facebook"></i></span>
+			<span><i className="linkedin fa-brands fa-linkedin-in"></i></span>
 			
 		</div>
 	</footer>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,68 +1,58 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			
+			message: null,
+			demo: [
+				{
+					title: "FIRST",
+					background: "white",
+					initial: "white"
+				},
+				{
+					title: "SECOND",
+					background: "white",
+					initial: "white"
+				}
+			],
 
 
-			
+
 		},
 		actions: {
-			
-			creat_user: async () => {
-
-				try {
-					const response = await fetch(process.env.BACKEND_URL+"/api/singup",{
-					method:"POST",
-					body:JSON.stringify({
-						
-					}),
-					headers:{
-						"content-type":"Aplication/json"
-
-					}
-
-					})
-
-					const body = await response.json()
-					const token =body.token
-					
-					localStorage.setItem("token",token)
-				} catch (error){
-					console.log(error)
-
-				}
-					
-
+			// Use getActions to call a function within a fuction
+			exampleFunction: () => {
+				getActions().changeColor(0, "green");
 			},
-			create_token: async () => {
 
-				try {
-					const response = await fetch(process.env.BACKEND_URL +"/api/login",{
-					method:"POST",
-					body:JSON.stringify({
-						"email":"hola",
-						"password":"hola"
-					}),
-					headers:{
-						"content-type":"Aplication/json"
-					}
-
-					})
-
-					const body = await response.json()
-					const token =body.token
-					
-					localStorage.setItem("token",token)
-				} catch (error){
-					console.log(error)
-
+			getMessage: async () => {
+				try{
+					// fetching data from the backend
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const data = await resp.json()
+					setStore({ message: data.message })
+					// don't forget to return something, that is how the async resolves
+					return data;
+				}catch(error){
+					console.log("Error loading message from backend", error)
 				}
+			},
+			changeColor: (index, color) => {
+				//get the store
+				const store = getStore();
 
+				//we have to loop the entire demo array to look for the respective index
+				//and change its color
+				const demo = store.demo.map((elm, i) => {
+					if (i === index) elm.background = color;
+					return elm;
+				});
+
+				//reset the global store
+				setStore({ demo: demo });
 			}
-			//get_app async ()=>{
-			
 
-		
+
+
 		}
 	};
 };

ec801da89584df28833cd575c52555782185461d
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-08-22 18:56:17 +0000

    Se aceptaron ambos cambios en el componente navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import { Login } from "./login.jsx";
 import "../../styles/navbar.css";
->>>>>>> b47d587da76f2e0edf63751fe94e69546d704c4e
 
 
 

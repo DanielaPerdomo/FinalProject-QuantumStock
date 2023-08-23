60cb844006e7dbc926652a1d34ab4917dc67c4dd
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-08-23 14:57:35 +0000

    Se cambio el estilo de los botones en general

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -17,13 +17,13 @@ export const Navbar = ({onLoginClick, onSignUpClick}) => {
 				</Link>
     			<div className="position-relative" id="navbarSupportedContent">
 					<Link to="/aboutUs">
-						<button type="button" className="btn btn-outline-light border border-0 m-2" href="#">Nosotros
+						<button type="button" className="btn btn-outline-light border border-0 m-2 buttonNav" href="#">Nosotros
 						</button>
 					</Link>
-						<button onClick={onLoginClick} type="button" className="btn btn-primary m-2">Iniciar Sesión&ensp;<i className="fa-solid fa-user"></i></button>
+						<button onClick={onLoginClick} type="button" className="btn btn-primary m-2 buttonNav">Iniciar Sesión&ensp;<i className="fa-solid fa-user"></i></button>
           
 					<Link to="/">
-						<button onClick={onSignUpClick} className="btn btn-outline-success text-light m-2" type="submit">
+						<button onClick={onSignUpClick} className="btn btn-outline-success text-light m-2 buttonNav" type="submit">
 							Registrarse&ensp;<i className="fa-solid fa-right-to-bracket"></i>
 						</button> 
 					</Link> 

7e53607beb4e2d722427ff8b92fe6b707cb7b449
Samuel Herrera <futbol.ingeniero@gmail.com>
2023-08-25 23:59:16 +0000

    Se arreglo el estilo del demo

--- src/front/js/pages/demo.js
+++ src/front/js/pages/demo.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Sidebar } from "../component/Sidebar.jsx";
 import { RequestInformation } from "../component/dashboardRequestedInfo.jsx";
-import { Navbardashboard} from "../component/Navbardashboard.jsx"
+import { Navbardashboard } from "../component/Navbardashboard.jsx"
 import { Context } from "../store/appContext";
 
 
@@ -10,25 +10,25 @@ export const Demo = () => {
 	const navigate = useNavigate();
 	const { store, actions } = useContext(Context);
 
-	useEffect(() =>{
-		if(store.token == undefined){   
+	useEffect(() => {
+		if (store.token == undefined) {
 			navigate("/")
-			
-		} 
+
+		}
 	}, [])
 
 	return (
-		<div className="information container-fluid m-0 p-0">
+		<div className="information container-fluid">
 			<div className="row">
-			  
-				<div className="col-3 m-0 p-0">
-				
-					 <Sidebar />    
+
+				<div className="col-2 p-0">
+
+					<Sidebar />
 				</div>
-				<div className="col-9 p-0">   
-				    <Navbardashboard/>  
-					<RequestInformation /> 
-					
+				<div className="col-10 m-0 p-0">
+					<Navbardashboard />
+					<RequestInformation />
+
 				</div>
 			</div>
 		</div>

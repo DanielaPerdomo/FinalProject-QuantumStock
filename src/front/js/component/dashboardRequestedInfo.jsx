e33eeaa284ae3fa093be4ca1680513db179f1907
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-08-26 03:39:02 +0000

    Se creo tabla y modal de agregar productos

--- src/front/js/component/dashboardRequestedInfo.jsx
+++ src/front/js/component/dashboardRequestedInfo.jsx
@@ -4,17 +4,86 @@ import "../../styles/dashboardPage.css";
 export const RequestInformation = () => {
     return (
 
-        <div className="card" style={{"width": "18rem"}}>
-            < img src = "..." className = "card-img-top" alt = "..." />
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+        <>
+        {/* TABLA DE MOSTRAR PRODUCTOS CARGADOE EN EL INVENTARIO */}
+            <table class="table table-responsive m-1">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody className="table-group-divider">
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            {/* FORMULARIO PARA AGREGAR PRODUCTOS */}
+            <div>
+                <button type="button" class="btn btn-outline-primary m-2" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
+                    <i class="fa-regular fa-square-plus"></i>
+                </button>
+                
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form>
+                                    <div class="mb-3">
+                                        <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                        <input type="text" class="form-control" id="recipient-name"/>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="message-text" class="col-form-label">Message:</label>
+                                        <textarea class="form-control" id="message-text"></textarea>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Send message</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-        </div >
+            </div>
+        </>
+
+
 
     )
 }
 
 
 
+{/* <div className="card" style={{"width": "18rem"}}>
+            < img src = "..." className = "card-img-top" alt = "..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+        </div > */}

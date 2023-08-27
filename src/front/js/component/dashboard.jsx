920f5f2c5a10522b589e8d228d6d04a9479e7dab
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-08-27 17:24:51 +0000

    funcionalidades botones

--- src/front/js/component/dashboard.jsx
+++ src/front/js/component/dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/dashboardPage.css";
 
-export const RequestInformation = () => {
+export const dashboard = () => {
   return (
 
     <>  <div>
--- src/front/js/pages/Dashboard.js
+++ src/front/js/pages/Dashboard.js
@@ -1,42 +1,47 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Sidebar } from "../component/Sidebar.jsx";
-import { RequestInformation } from "../component/dashboardRequestedInfo.jsx";
+//import { RequestInformation } from "../component/dashboardRequestedInfo.jsx";
 import { Navbardashboard } from "../component/Navbardashboard.jsx"
 import { Context } from "../store/appContext.js";
 import { Almacen } from "./Almacen.js";
+import { Dashboard } from "../component/dashboard.jsx";
 
-export const Dashboard = ( ) => {
+export const Dashboard = () => {
 	const navigate = useNavigate();
 	const { store, actions } = useContext(Context);
-	
+
 	const [showAlmacen, setShowAlmacen] = useState(false);
 	const [showProduct, setShowProduct] = useState(false);
 	const [showHome, setShowHome] = useState(true);
-    /*almacen*/ 
-	const handlealmacenClick = () =>{
-        if (showAlmacen==false) {
-		 setShowAlmacen(true);
-		setShowProduct(false);
+	/*almacen*/
+	const handlealmacenClick = () => {
+		if (showAlmacen == false) {
+			setShowAlmacen(true);
+			setShowProduct(false);
+			setShowHome(false);
+		}
 
-	}
-		
 	};
 
-	const handleproductClick = () =>{
+	const handleproductClick = () => {
 
-		if (showProduct==false) {
+		if (showProduct == false) {
 			setShowProduct(true);
 			setShowAlmacen(false);
-			
+			setShowHome(false);
 		}
 	};
 
-  const handlehomeClick = () =>{
+	const handlehomeClick = () => {
 
+		if (showHome == false) {
+		setShowHome(true)
+		setShowAlmacen(false);
+		setShowProduct(false);
 
-
-  }
+	}
+	}
 	useEffect(() => {
 		if (store.token == undefined) {
 			navigate("/")
@@ -50,17 +55,18 @@ export const Dashboard = ( ) => {
 
 				<div className=" p-0 col-lg-3 col-4">
 
-					<Sidebar onAlmacenClick={handlealmacenClick} 
-					         onProduckClick={handleproductClick}
-							 onHomeClick={handlehomeClick}
+					<Sidebar onAlmacenClick={handlealmacenClick}
+						onProduckClick={handleproductClick}
+						onHomeClick={handlehomeClick}
 					/>
 				</div>
 				<div className=" m-0 p-0 col-lg-9 col-8">
-				      {    }
+					{ }
 					<Navbardashboard />
-					
-                    {showAlmacen && <Almacen/>}
-					{ showProduct && <RequestInformation /> }
+
+					{showAlmacen && <Almacen />}
+					{/* { showProduct && <RequestInformation /> } */}
+					{showHome && < Dashboard />}
 
 
 				</div>

948f942fcd3e152c2aad5161952bbeb39985e0cc
DanielaPerdomo <98133250+DanielaPerdomo@users.noreply.github.com>
2023-08-27 13:38:26 +0000

    Creacion de relaciones en las bases de datos y rutas

--- src/api/models.py
+++ src/api/models.py
@@ -17,32 +17,6 @@ class User(db.Model):
     # Relacion con Product
     Product = db.relationship("Product", back_populates="user")
 
-class Stock(db.Model):
-    __tablename__ = "stock"
-    id = db.Column(db.Integer, primary_key=True)
-    company_name = db.Column(db.String(250), unique=False, nullable=False)
-    address = db.Column(db.String(250), unique=True, nullable=False)
-    rif = db.Column(db.String(250), unique=True, nullable=False)
-    # Relacion con el user
-    user_id = db.Column(db.ForeignKey("user.id"), nullable=True)
-    user = db.relationship("User", back_populates="Stock")
-    # Relacion con Product
-    Product = db.relationship("Product", back_populates="stock")
-
-class Product(db.Model):
-    __tablename__ = "product"
-    id = db.Column(db.Integer, primary_key=True)
-    product_name = db.Column(db.String(100), unique=False, nullable=False)
-    description = db.Column(db.String(250), unique=False, nullable=False)
-    item = db.Column(db.Integer, unique=False, nullable=False)
-    price = db.Column(db.Integer, unique=False, nullable=False)
-    admission_date = db.Column(db.String(10), unique=False, nullable=False)
-    # relacion con stock
-    stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'), nullable=False)
-    stock = db.relationship("Stock", back_populates="Product")
-    # Relacion con user
-    user_id = db.Column(db.ForeignKey("user.id"), nullable=True)
-    user = db.relationship("User", back_populates="Product")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -56,3 +30,41 @@ class Product(db.Model):
             "rol_company": self.rol_company
             # do not serialize the password, its a security breach
         }
+
+class Stock(db.Model):
+    __tablename__ = "stock"
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(250), unique=False, nullable=False)
+    address = db.Column(db.String(250), unique=True, nullable=False)
+    rif = db.Column(db.String(250), unique=True, nullable=False)
+    # Relacion con el user
+    user_id = db.Column(db.ForeignKey("user.id"), nullable=True)
+    user = db.relationship("User", back_populates="Stock")
+    # Relacion con Product
+
+
+    def __repr__(self):
+        return f'<Stock {self.company_name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "company_name": self.company_name,
+            "address": self.address,
+            "rif": self.rif,
+            # do not serialize the password, its a security breach
+        }
+
+class Product(db.Model):
+    __tablename__ = "product"
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(100), unique=False, nullable=False)
+    description = db.Column(db.String(250), unique=False, nullable=False)
+    item = db.Column(db.Integer, unique=False, nullable=False)
+    price = db.Column(db.Integer, unique=False, nullable=False)
+    admission_date = db.Column(db.String(15), unique=False, nullable=False)
+    # Relacion con User
+    user_id = db.Column(db.ForeignKey("user.id"))
+    user = db.relationship("User", back_populates="product")
+    # Relacion con Stock
+    

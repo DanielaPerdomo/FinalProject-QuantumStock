30aee8c6ce74b938bec26ebe91650c138d6cb56d
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-08-30 17:20:32 +0000

    agregado de cards informativas

--- src/front/js/component/dashboardHome.jsx
+++ src/front/js/component/dashboardHome.jsx
@@ -3,17 +3,34 @@ import "../../styles/dashboardPage.css";
 
 export const DashboardHome = () => {
   return (
-    <div>
-      <div className="card">
-        <h5 className="card-header">Featured</h5>
+    <div className="d-flex justify-content-center m-2 ">
+    
+      <div className="card text-bg-primary  m-2" style={{"max-width": "18rem"}}>
+        <div className="card-header">Almacen</div>
         <div className="card-body">
-          <h5 className="card-title">Special title treatment</h5>
-          <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">Primary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
-      <div className="card text-bg-primary mb-3" style={{"max-width": "18rem"}}>
-        <div className="card-header">Header</div>
+
+      <div className="card text-bg-dark  m-2" style={{"max-width": "18rem"}}>
+        <div className="card-header">Productos</div>
+        <div className="card-body">
+          <h5 className="card-title">Primary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div className="card text-bg-secondary  m-2" style={{"max-width": "18rem"}}>
+        <div className="card-header">Clientes</div>
+        <div className="card-body">
+          <h5 className="card-title">Primary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div className="card text-bg-info  m-2" style={{"max-width": "18rem"}}>
+        <div className="card-header">Proveedores</div>
         <div className="card-body">
           <h5 className="card-title">Primary card title</h5>
           <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>

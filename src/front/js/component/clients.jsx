805ffc60b6f9f536e71938b7854947e3addb2109
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-09-01 20:08:36 +0000

    una parte de la funcionabilidad del componente client

--- src/front/js/component/clients.jsx
+++ src/front/js/component/clients.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useContext} from "react";
+import React, { useState, useContext } from "react";
 import { ModalClient } from "./modalClient.jsx";
 import { Context } from "../store/appContext";
 import { Toaster, toast } from 'sonner'
@@ -13,52 +13,52 @@ export const Clients = () => {
     phone_client: "",
     address_client: "",
     rif_client: "",
-})
-const handleInfo = (event) => {
-  setClient({
+  })
+  const handleInfo = (event) => {
+    setClient({
       ...client,
       [event.target.name]: event.target.value //Fin de codigo de Jose
-  })
-}
+    })
+  }
 
-const resetForm = () => {
-  setProducts({
-    name_client: "",
-    email_client: "",
-    phone_client: "",
-    address_client: "",
-    rif_client: ""
-  });
+  const resetForm = () => {
+    setProducts({
+      name_client: "",
+      email_client: "",
+      phone_client: "",
+      address_client: "",
+      rif_client: ""
+    });
 
-};
-  async function createProduct(event) {
+  };
+  async function createClient(event) {
     event.preventDefault();
 
     try {
-        const opts = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                authorization: `Bearer ${store.token}`
-            },
-            body: JSON.stringify(client)
+      const opts = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${store.token}`
+        },
+        body: JSON.stringify(client)
 
-        };
+      };
 
-        const resp = await fetch(process.env.BACKEND_URL + "api/client", opts);
-        if (resp.ok) {
-            resetForm();
-            actions.getClient()
-            toast.success('El cliente se creo exitosamente')
-            return await resp.json();
-        } else {
-            return toast.error("El cliente no pudo crearse")
-        }
+      const resp = await fetch(process.env.BACKEND_URL + "api/client", opts);
+      if (resp.ok) {
+        resetForm();
+        actions.getClient()
+        toast.success('El cliente se creo exitosamente')
+        return await resp.json();
+      } else {
+        return toast.error("El cliente no pudo crearse")
+      }
     } catch (error) {
 
-        console.error("There was an Error!!!", error);
+      console.error("There was an Error!!!", error);
     };
-};
+  };
 
 
 
@@ -70,32 +70,48 @@ const resetForm = () => {
           <h1 className=" text-dark "><i className="fa-solid fa-person-circle-check"></i><span className="m-3">Clientes</span></h1>
         </div>
 
-        <div className="card " style={{ maxWidth: "18rem" }}>
-          <div>
-            <img src="" className="card-img-top" alt="..."></img>
-          </div>
-          <div className="card-body "  >
-            <div className="d-flex justify-content-center">
-              <h5 className="card-title  m-1 fs-6">Algo</h5>
+
+        {store.cliente.map((item, index) => {
+
+          return (
+
+            <div className="card " style={{ maxWidth: "18rem" }}>
+
+              <div className="card-body "  >
+                <div className="d-flex justify-content-center">
+                  <h5 className="card-title  m-1 fs-6">Algo</h5>
+                </div>
+                <p></p>
+                  <p className="card-text  m-1 fs-6 ">Nombre del cliente: Juan  </p>
+                  <p className="card-text m-1 fs-6 ">Email del cliente: Juan@juan.com</p>
+                  <p className="card-text  m-1 fs-6">Telefono del cliente: </p>
+                  <p className="card-text  m-1 fs-6">Dirección del cliente: </p>
+                  <p className="card-text  m-1 fs-6">Rif del cliente: </p>
+                  <div className="d-flex justify-content-between m-1 ">
+                    <button className="btn border border-primary"><i className="fa-regular fa-pen-to-square"></i> &ensp; Editar</button>
+                    <button className="btn border border-danger"><i className="fa-regular fa-trash-can"></i> &ensp; Eliminar</button>
+                  </div>
+              </div>
             </div>
-            <p className="card-text  m-1 fs-6 ">Nombre del cliente:Juan  </p>
-            <p className="card-text m-1 fs-6 ">Email del cliente: Juan@juan.com</p>
-            <p className="card-text  m-1 fs-6">Telefono del cliente: </p>
-            <p className="card-text  m-1 fs-6">Dirección del cliente: </p>
-            <p className="card-text  m-1 fs-6">Rif del cliente: </p>
-            <div className="d-flex justify-content-between m-1 ">
-              <button className="btn border border-primary"><i className="fa-regular fa-pen-to-square"></i> &ensp; Editar</button>
-              <button className="btn border border-danger"><i className="fa-regular fa-trash-can"></i> &ensp; Eliminar</button>
-            </div>
-          </div>
-        </div>
+
+          )
+
+        })
+        }
+
+
         {/* FIN DEL CARD DE CLIENTE */}
 
         {/* INICIO MODAL NUEVO CLIENTE */}
-        <button type="button" className="btn btn-outline-primary m-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="btn btn-outline-primary m-2" data-bs-toggle="modal" data-bs-target="#createClient">
           <i className="fa-regular fa-square-plus"></i>
         </button>
-        <ModalClient />
+
+
+        <ModalClient
+          id={"createClient"}
+
+        />
       </div>
     </>
 

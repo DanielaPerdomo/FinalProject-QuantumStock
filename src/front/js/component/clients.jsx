3786888d542060c47d34dbe5eb77dfc256a82ac3
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-09-01 17:06:13 +0000

    agregado de solicitud fetch de clientes (GET,POST,DELETE,PUT)

--- src/front/js/component/clients.jsx
+++ src/front/js/component/clients.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React,{useState,useContext} from "react";
 import { ModalClient } from "./modalClient.jsx";
+import { Context } from "../store/appContext";
+import { Toaster, toast } from 'sonner'
+
 
 export const Clients = () => {
+  const { store, actions } = useContext(Context);
 
+  const [client, setClient] = useState({
+    name_client: "",
+    email_client: "",
+    phone_client: "",
+    address_client: "",
+    rif_client: "",
+})
+const handleInfo = (event) => {
+  setClient({
+      ...client,
+      [event.target.name]: event.target.value //Fin de codigo de Jose
+  })
+}
 
+const resetForm = () => {
+  setProducts({
+    name_client: "",
+    email_client: "",
+    phone_client: "",
+    address_client: "",
+    rif_client: ""
+  });
+
+};
+  async function createProduct(event) {
+    event.preventDefault();
+
+    try {
+        const opts = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                authorization: `Bearer ${store.token}`
+            },
+            body: JSON.stringify(client)
+
+        };
+
+        const resp = await fetch(process.env.BACKEND_URL + "api/client", opts);
+        if (resp.ok) {
+            resetForm();
+            actions.getClient()
+            toast.success('El cliente se creo exitosamente')
+            return await resp.json();
+        } else {
+            return toast.error("El cliente no pudo crearse")
+        }
+    } catch (error) {
+
+        console.error("There was an Error!!!", error);
+    };
+};
 
 
 
@@ -14,9 +69,7 @@ export const Clients = () => {
       </div>
 
       <div className="card " style={{ maxWidth: "18rem" }}>
-        <div>
-          <img src="" className="card-img-top" alt="..."></img>
-        </div>
+        
         <div className="card-body "  >
           <div className="d-flex justify-content-center">
             <h5 className="card-title  m-1 fs-6">Algo</h5>
